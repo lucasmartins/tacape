b6c668702917c13e26fe60a416a7206b7315fe86
Lucas Neves Martins <lucasmartins@railsnapraia.com>
2013-06-08 22:20:43 -0300

    wip, externalizing dns

--- lib/tacape/cli.rb
+++ lib/tacape/cli.rb
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 module Tacape
   load "tacape/tools/helpers/json_config.rb"
-  load "tacape/tools/helpers/os_support.rb"
+  #load "tacape/tools/helpers/os_support.rb"
   load "tacape/tools/dns.rb"
   load "tacape/tools/gitrepo.rb"
   
